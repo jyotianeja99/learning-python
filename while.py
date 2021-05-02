18f18325fde1996f000913f7fa46b172caf11bb5
jyotianeja99 <jyotianeja99@gmail.com>
2021-05-02 22:57:54 +0530

    While loop syntax

---
+++ while.py
@@ -0,0 +1,10 @@
+name = ''
+i = 0
+while name != 'Jyoti' and i<6:
+    print('Please enter your name.!')
+    name = input()
+    i = i+1
+    if name =='Jyoti':
+        print('Welcome,'+name)
+        
+print('Please try later and Have a good day.!')
